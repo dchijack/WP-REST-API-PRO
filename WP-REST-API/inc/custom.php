50f761589340ff24665a3233865cb0ed9fe49185
Chi.Jack <linjian2005@gmail.com>
2018-10-06 12:32:08 +0800

    更新自定义文章 API 输出

--- WP-REST-API/inc/custom.php
+++ WP-REST-API/inc/custom.php
@@ -64,7 +64,79 @@ if (wp_get_option('custom_menu')) {
 			'index.php?post_type='.$singular.'&p=$matches[1]&cpage=$matches[2]',
 			'top'
 		);
-	}); 
+	});
+	add_filter( 'rest_prepare_'.wp_get_option('custom_singular'), function ($data, $post, $request) {
+		global $wpdb;
+		$_data = $data->data;    
+		$post_id = $post->ID;
+		$post_views = (int)get_post_meta($post_id, 'views',true);
+		$post_thumbnail = get_post_thumbnail($post_id);
+		$post_comment = wp_count_comments($post_id);
+		//$content = $post->post_content;
+		$category = get_the_category($post_id);
+		$categoryId=$category[0]->term_id;
+		$next_post = get_next_post($categoryId, '', 'category');
+		$previous_post = get_previous_post($categoryId, '', 'category');
+		$sql_thumbs = $wpdb->prepare("SELECT COUNT(1) FROM ".$wpdb->postmeta." where meta_value='thumbs' and post_id=%d",$post_id);
+		$post_thumbs = $wpdb->get_var($sql_thumbs);
+		$params = $request->get_params();
+		if ( isset( $params['id'] )) {
+			$sql=$wpdb->prepare("SELECT meta_key , (SELECT ID from ".$wpdb->users." WHERE user_login=substring(meta_key,2)) as userID FROM ".$wpdb->postmeta." where meta_value='thumbs' and post_id=%d",$post_id);
+			$zanuser = $wpdb->get_results($sql);
+			$avatarurls =array();
+			foreach ($zanuser as $userid) {
+				$_avatarurl['avatar'] = get_user_meta( $userid->userID, 'wxavatar', true);
+				$avatarurls[] = $_avatarurl;       
+			}
+		} else {
+			if (get_setting_option('post_content')) { unset($_data['content'] ); }  	
+		}
+		//$_data['content']['rendered'] = $content;
+		$_data['category'] = $category[0]->cat_name;
+		$_data['comments'] = $post_comment->total_comments;
+		$_data['thumbses'] = $post_thumbs;
+		if (get_setting_option('post_meta')) {
+			$_data["thumbnail"] = $post_thumbnail;
+			$_data["views"] = $post_views;
+		} else {
+			$_data["meta"]["thumbnail"] = $post_thumbnail;
+			$_data["meta"]["views"] = $post_views;
+			$meta = get_setting_option('meta_list');
+			if (!empty($meta)) {
+				foreach ($meta as $meta=>$key) {
+					$_data["meta"][$key] = get_post_meta( $post_id, $key ,true );
+				}
+			}
+			//-----------------------------------------------------------
+		}
+		$_data['avatar']= $avatarurls;
+		if (get_setting_option('post_prev')) {
+			$_data['next_id'] = !empty($next_post->ID)?$next_post->ID:null;
+			$_data['next_title'] = !empty($next_post->post_title)?$next_post->post_title:null;
+			$_data['previous_id'] = !empty($previous_post->ID)?$previous_post->ID:null;
+			$_data['previous_title'] = !empty($previous_post->post_title)?$previous_post->post_title:null;
+		}
+		if (get_setting_option('post_excerpt')) {unset($_data['excerpt']);}
+		if (get_setting_option('post_meta')) {unset($_data['meta']); }
+		if (get_setting_option('post_format')) {unset($_data['format']);}
+		if (get_setting_option('post_type')) {unset($_data['type']);}
+		if (get_setting_option('post_author')) {unset($_data['author']);} else {$_data['author'] = get_the_author_meta('display_name',$post->post_author);} // 显示文章作者昵称
+		unset($_data['featured_media']);
+		unset($_data['ping_status']);
+		unset($_data['template']);
+		unset($_data['slug']);
+		unset($_data['modified_gmt']);
+		unset($_data['date_gmt']);
+		unset($_data['guid']);
+		unset($_data['curies']);
+		unset($_data['modified']);
+		unset($_data['status']);
+		unset($_data['comment_status']);
+		unset($_data['sticky']);    
+		unset($_data['_links']['self']); 
+		$data->data = $_data; 
+		return $data; 
+	},10, 3 );
 }
 if (wp_get_option('custom_category')) {
 	// 自定义文章类型分类
@@ -97,6 +169,58 @@ if (wp_get_option('custom_category')) {
 		);
 		register_taxonomy( $category, array( $singular ), $args );
 	}, 30 );
+	add_filter( 'rest_prepare_'.wp_get_option('custom_category'), function ($data, $item, $request) {
+		$category_cover_image = '';
+		$temp = '';
+		$term_id = $item->term_id;
+		$args = array('category'=>$term_id,'numberposts' => 1);
+		$posts = get_posts($args);
+		if (!empty($posts)) {
+			$recent_date = $posts[0]->post_date;
+		} else {
+			$recent_date = '暂无更新';
+		}
+		if($temp = get_term_meta($item->term_id,'cover',true)) {
+			$category_cover_image = $temp; 
+		}
+		$data->data['cover'] = $category_cover_image;
+		$data->data['date'] = $recent_date; 	
+		return $data;
+	}, 10, 3 );
+	/*********   给分类添加封面 *********/
+	add_action( wp_get_option('custom_category').'_add_form_fields', function () {
+		wp_nonce_field( basename( __FILE__ ), 'the_category_term_cover_nonce' ); ?>
+		<div class="form-field the-category-term-cover-wrap">
+			<label for="the-category-cover">封面</label>
+			<input type="url" name="the_category_term_cover" id="the-category-cover"  class="type-image regular-text" data-default-cover="" />
+		</div>
+	<?php });
+	add_action( wp_get_option('custom_category').'_edit_form_fields', function ( $term ) {
+		$default = '';
+		$cover   = get_term_meta( $term->term_id, 'cover', true );
+		if (!$cover)
+		   $cover = $default; 
+	?>
+		<tr class="form-field the-category-term-cover-wrap">
+			<th scope="row"><label for="the-category-cover">封面 </label></th>
+			<td>
+				<?php echo wp_nonce_field( basename( __FILE__ ), 'the_category_term_cover_nonce' ); ?>
+				<input type="url" name="the_category_term_cover" id="the-category-cover" class="type-image regular-text" value="<?php echo esc_attr( $cover ); ?>" data-default-cover="<?php echo esc_attr( $default ); ?>" />
+			</td>
+		</tr>
+	<?php });
+	add_action( 'create_'.wp_get_option('custom_category'), 'save_custom_category_cover' );
+	add_action( 'edit_'.wp_get_option('custom_category'),'save_custom_category_cover' );
+	function save_custom_category_cover( $term_id ) {
+		if ( ! isset( $_POST['the_category_term_cover_nonce'] ) || ! wp_verify_nonce( $_POST['the_category_term_cover_nonce'], basename( __FILE__ ) ) )
+			return;
+		$cover = isset( $_POST['the_category_term_cover'] ) ? $_POST['the_category_term_cover'] : '';
+		if ( '' === $cover ) {
+			delete_term_meta( $term_id, 'cover' );
+		} else {
+			update_term_meta( $term_id, 'cover', $cover );
+		}
+	}
 }
 if (wp_get_option('custom_tags')) {
 	// 注册自定义文章类型分类标签
